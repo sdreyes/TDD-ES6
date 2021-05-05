8b7f5e3d231c78884acd3e8b68448d56ce49e968
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2021-05-05 12:05:56 -0400

    Modify getLetterCount to pass third test

--- src/letter-count.js
+++ src/letter-count.js
@@ -8,8 +8,10 @@ export const getLetterCount = string => {
   letters.forEach(letter => {
     if (!letterCount[letter]) {
       letterCount[letter] = 1;
+    } else {
+      letterCount[letter] += 1;
     }
   })
-  
+
   return letterCount;
 }

9b9d3748ae9899c087cc0def75b009d1a36ca651
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2021-05-06 10:42:52 -0400

    Create test helpers to isolate MongoDB read/write/drop functionality for integration tests

--- integration-tests/src/db.test.js
+++ integration-tests/src/db.test.js
@@ -1,18 +1,9 @@
-import { MongoClient } from 'mongodb';
 import { expect } from 'chai';
 import { getUserByUsername } from './db';
+import { getDatabaseData, resetDatabase, setDatabaseData } from './test-helpers';
 
 describe('getUserByUsername', () => {
   it('gets the correct user from the database given a username', async () => {
-    const client = await MongoClient.connect(
-      'mongodb://localhost:27017/TEST_DB',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
-    const db = client.db('TEST_DB');
-
     const fakeData = [
       {
         id: '123',
@@ -26,11 +17,10 @@ describe('getUserByUsername', () => {
       }
     ];
 
-    await db.collection('users').insertMany(fakeData);
+    await setDatabaseData('users', fakeData);
     const actual = await getUserByUsername('abc');
-    const finalDBState = await db.collection('users').find().toArray();
-    await db.dropDatabase();
-    client.close();
+    const finalDBState = await getDatabaseData('users');
+    await resetDatabase();
 
     const expected = {
       id: '123',

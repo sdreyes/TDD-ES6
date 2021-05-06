38e418c9c07b7a4a1a73d7854d5fdff792b7a7ec
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2021-05-06 09:34:20 -0400

    Initalize files for integration test exercises

--- unit-tests/src/anagrams.test.js
+++ unit-tests/src/anagrams.test.js
@@ -5,48 +5,48 @@ describe('isAnagram - basic functionality', () => {
   it('returns false when no strings are passed', () => {
     const expected = false;
     const actual = isAnagram();
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 
   it('returns false when empty strings are passed', () => {
     const expected = false;
     const actual = isAnagram('', '');
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 
   it('returns false when strings of two different lengths are passed', () => {
     const expected = false;
     const actual = isAnagram('the', 'then');
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 
   it('returns false when strings of the same length but different letter compositions are passed', () => {
     const expected = false;
     const actual = isAnagram('cat', 'hat');
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 
   it('returns true when strings of the same length and letter composition are passed', () => {
     const expected = true;
     const actual = isAnagram('state', 'taste');
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 
   it('returns true when strings of the same length and letter composition are passed, even when the letter casings vary', () => {
     const expected = true;
     const actual = isAnagram('state', 'TASTE');
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 
   it('returns true when strings of the same length and letter composition are passed, even if there are spaces in one but not the other', () => {
     const expected = true;
     const actual = isAnagram('conversation', 'voices rant on');
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 
   it('returns true when strings of the same length and letter composition are passed, even if there are non-alphanumeric characters in one but not the other', () => {
     const expected = true;
     const actual = isAnagram('!@#$%^&*()-=_+[]{} ;:`~\'",./?<>listen', 'silent');
-    expect(actual).to.deep.equal(expected);
+    expect(actual).to.equal(expected);
   });
 })

1256f99ab4fd3084b3edbeed3ef276ce208f1a34
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2021-05-06 13:28:02 -0400

    Add failing test for /users/:username endpoint when the user is not found

--- integration-tests/src/server.test.js
+++ integration-tests/src/server.test.js
@@ -36,5 +36,15 @@ describe('GET /users/:username', () => {
       .expect(fakeError);
 
     stub.restore();
-  })
+  });
+
+  it('returns the appropriate response when the user is not found', async () => {
+    const stub = sinon.stub(db, 'getUserByUsername')
+      .resolves(null);
+
+    await request(app).get('/users/abc')
+      .expect(404)
+
+    stub.restore();
+  });
 });
